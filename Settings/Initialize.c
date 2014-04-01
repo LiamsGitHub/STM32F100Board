7ee8c78eabac2114fc96e58d9d7d12af8445377b
Liam G <Liam@lg-mac-mini.gateway.2wire.net>
2014-03-31 22:12:41 -0700

    MacMini to Laptop v1

--- Settings/Initialize.c
+++ Settings/Initialize.c
@@ -6,9 +6,9 @@ void SystemInit (void)
 	volatile uint32_t	regState;
 
 	  //Set the Clock config register to: SYSCLK=PLL, PLL Source=HSI clock, PLL is x6, MCO=PLL/2 output at MCO pin 29
-	RCC_CFGR=0x4100002; // Sets MCO to output SYSCLK, PLL x6, HSI clock as PLL source, PLL as SYSCLK source
+	//RCC_CFGR=0x4100002; // Sets MCO to output SYSCLK, PLL x6, HSI clock as PLL source, PLL as SYSCLK source
 
-	  //RCC_CFGR=0x5000000; //MCO output of HSI clock (8MHz)
+	  RCC_CFGR=0x5000000; //MCO output of HSI clock (8MHz)
 	  //RCC_CFGR=0x4000000; //MCO output of SYSCLK
 
 	regState=RCC_CR;
